2aba00b95c9e8239842a939c962fec25a1975075
karia <karia@side2.net>
2023-12-23 05:54:19 +0900

    change homebrew path if arm64 or x86_64

--- setup.sh
+++ setup.sh
@@ -10,7 +10,11 @@ EOS
 
 if ! (type "brew" > /dev/null 2>&1); then
   /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-  echo "eval \"\$(/opt/homebrew/bin/brew shellenv)\"" >> ~/.zprofile
+  if [ "$(uname -m)" == 'x86_64' ]; then
+    echo "eval \"\$(/opt/brew/bin/brew shellenv)\"" >> ~/.zprofile
+  elif [ "$(uname -m)" == 'arm64' ]; then
+    echo "eval \"\$(/opt/homebrew/bin/brew shellenv)\"" >> ~/.zprofile
+  fi
 fi
 
 if ! (type "ansible" > /dev/null 2>&1); then
